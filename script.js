426b5e1f0bacdd77ce8eaf65b17611b94a32c4f6
mkidee <mckaymemmott@gmail.com>
2023-03-27 10:36:51 -0600

    got the if statement to work with dayjs, classes should change color depending on the time. Now I will add each statement for every time slot

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -9,7 +11,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-
+  //this variable is used for the date display
   var today = document.querySelector('#currentDay');
 
   // this is an ex of trying to do all classes at once
@@ -19,24 +21,42 @@ $(function () {
   toDoText.textContent = toDos;
 
 // this is an example of using ids for each individual piece
-  var save10 = document.querySelector('#btn10');
-  var textin10 = document.querySelector('#text10');
-  var todo10 = localStorage.getItem("todo10");
-  textin10.textContent = todo10;
+  // var save10 = document.querySelector('#btn10');
+  // var textin10 = document.querySelector('#text10');
+  // var todo10 = localStorage.getItem("todo10");
+  // textin10.textContent = todo10;
 
-  var nm12 = '0';
+
+
+  
+
+
+
   
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  
+
+
+
+  text12 = document.querySelector('#hour-12');
+  var nm12 = '12';
 
   function timeCheck12() {
-    if (nm12 < dayjs().format('H').value) {
-      document.getElementById('#hour-12').className = '.future';
-      // document.getElementById('hour-12').setAttribute('class', 'future');
+    if (nm12 < dayjs().format('H')) {
+      document.getElementById('#hour-12').className = 'past row time-block';
+      // document.getElementById('#hour-12').setAttribute('class', '.future');
+      return
+    }
+    else if (nm12 == dayjs().format('H')) {
+      document.getElementById('#hour-12').className = 'present row time-block';   
+      return   
+    }
+    else if (nm12 > dayjs().format('H')){
+      document.querySelector('#hour-12').className = 'future row time-block';
+      return
     }
   }
 
@@ -51,17 +71,20 @@ $(function () {
     localStorage.setItem("toDos", toDos);
   }); 
 
-  save10.addEventListener("click", function() {
-    textin10.textContent = todo10;
-    localStorage.setItem("todo10", JSON.stringify(todo10));
-    render10();
-  });
 
-  function render10() {
-    var display10 = JSON.parse(localStorage.getItem("todo10"));
-    textin10.textContent = display10;
-    // textin10.textContent = JSON.parse(localStorage.getItem("todo10"));
-  }
+
+
+  // save10.addEventListener("click", function() {
+  //   textin10.textContent = todo10;
+  //   localStorage.setItem("todo10", JSON.stringify(todo10));
+  //   render10();
+  // });
+
+  // function render10() {
+  //   var display10 = JSON.parse(localStorage.getItem("todo10"));
+  //   textin10.textContent = display10;
+  //   // textin10.textContent = JSON.parse(localStorage.getItem("todo10"));
+  // }
 
   // TODO: Add code to display the current date in the header of the page.
   today.textContent = dayjs().format('MMMM D, YYYY');
