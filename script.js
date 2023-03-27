4a3e4079ef6e3f4c58ce6a5469f93f1579626253
mkidee <mckaymemmott@gmail.com>
2023-03-27 12:50:42 -0600

    added in the rest of the classes, removed an excess variable I created. All are working except the hour9 class. About to redo.

--- script.js
+++ script.js
@@ -28,7 +28,6 @@ $(function () {
 
 
 
-
   
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -37,47 +36,49 @@ $(function () {
   // current hour in 24-hour time?
 
 
-  var text9 = document.querySelector('#hour-9');
-  var nm9 = '9';
+
+  var text10 = document.querySelector('#hour-10');
+  var nm10 = '10';
 
   function timeCheck9() {
-    if (nm9 < dayjs().format('H')) {
-      text9.classList.add('past')
-      // document.getElementById('#hour-9').className = 'past row time-block';
-      // document.getElementById('#hour-12').setAttribute('class', '.future');
+    if (nm10 < dayjs().format('H')) {
+      text10.classList.add('past')
+      return
     }
-    else if (nm9 === dayjs().format('H')) {
-      text9.classList.add('present')
-      // document.getElementById('#hour-9').className = 'present row time-block';   
+    else if (nm10 === dayjs().format('H')) {
+      text10.classList.add('present')
+      return   
     }
-    else if (nm9 > dayjs().format('H')){
-      text9.classList.add('future')
-      // document.querySelector('#hour-9').className = 'future row time-block';
+    else if (nm10 > dayjs().format('H')){
+      text10.classList.add('future')
+      return
     }
   }
 
 
 
-  // text10 = document.querySelector('#hour-10');
-  // var nm10 = '10';
+  var text10 = document.querySelector('#hour-10');
+  var nm10 = '10';
 
-  // function timeCheck10() {
-  //   if (nm10 < dayjs().format('H')) {
-  //     document.getElementById('#hour-10').className = 'past row time-block';
-  //     // document.getElementById('#hour-12').setAttribute('class', '.future');
-  //   }
-  //   else if (nm10 === dayjs().format('H')) {
-  //     document.getElementById('#hour-10').className = 'present row time-block';   
-  //   }
-  //   else {
-  //     document.querySelector('#hour-10').className = 'future row time-block';
-  //   }
-  // }
+  function timeCheck10() {
+    if (nm10 < dayjs().format('H')) {
+      text10.classList.add('past')
+      return
+    }
+    else if (nm10 === dayjs().format('H')) {
+      text10.classList.add('present')
+      return   
+    }
+    else if (nm10 > dayjs().format('H')){
+      text10.classList.add('future')
+      return
+    }
+  }
 
-  console.log(dayjs().format('H'));
+
 
   var text11 = document.querySelector('#hour-11');
-  var nm11 = '12';
+  var nm11 = '11';
 
   function timeCheck11() {
     if (nm11 < dayjs().format('H')) {
@@ -121,10 +122,128 @@ $(function () {
     }
   }
 
+  var text13 = document.querySelector('#hour-1');
+  var nm13 = '13';
+
+  function timeCheck13() {
+    if (nm13 < dayjs().format('H')) {
+      text13.classList.add('past')
+      // document.getElementById('#hour-12').className = 'past row time-block';
+      // document.getElementById('#hour-12').setAttribute('class', '.future');
+      return
+    }
+    else if (nm13 === dayjs().format('H')) {
+      text13.classList.add('present')
+      // document.getElementById('#hour-12').className = 'present row time-block';   
+      return   
+    }
+    else if (nm13 > dayjs().format('H')){
+      text13.classList.add('future')
+      // document.querySelector('#hour-12').className = 'future row time-block';
+      return
+    }
+  }
+
+  var text14 = document.querySelector('#hour-2');
+  var nm14 = '14';
+
+  function timeCheck14() {
+    if (nm14 < dayjs().format('H')) {
+      text14.classList.add('past')
+      // document.getElementById('#hour-12').className = 'past row time-block';
+      // document.getElementById('#hour-12').setAttribute('class', '.future');
+      return
+    }
+    else if (nm14 === dayjs().format('H')) {
+      text14.classList.add('present')
+      // document.getElementById('#hour-12').className = 'present row time-block';   
+      return   
+    }
+    else if (nm14 > dayjs().format('H')){
+      text14.classList.add('future')
+      // document.querySelector('#hour-12').className = 'future row time-block';
+      return
+    }
+  }
+
+  var text15 = document.querySelector('#hour-3');
+  var nm15 = '15';
+
+  function timeCheck15() {
+    if (nm15 < dayjs().format('H')) {
+      text15.classList.add('past')
+      // document.getElementById('#hour-12').className = 'past row time-block';
+      // document.getElementById('#hour-12').setAttribute('class', '.future');
+      return
+    }
+    else if (nm15 === dayjs().format('H')) {
+      text15.classList.add('present')
+      // document.getElementById('#hour-12').className = 'present row time-block';   
+      return   
+    }
+    else if (nm15 > dayjs().format('H')){
+      text15.classList.add('future')
+      // document.querySelector('#hour-12').className = 'future row time-block';
+      return
+    }
+  }
+
+  var text16 = document.querySelector('#hour-4');
+  var nm16 = '16';
+
+  function timeCheck16() {
+    if (nm16 < dayjs().format('H')) {
+      text16.classList.add('past')
+      // document.getElementById('#hour-12').className = 'past row time-block';
+      // document.getElementById('#hour-12').setAttribute('class', '.future');
+      return
+    }
+    else if (nm16 === dayjs().format('H')) {
+      text16.classList.add('present')
+      // document.getElementById('#hour-12').className = 'present row time-block';   
+      return   
+    }
+    else if (nm16 > dayjs().format('H')){
+      text16.classList.add('future')
+      // document.querySelector('#hour-12').className = 'future row time-block';
+      return
+    }
+  }
+
+  var text17 = document.querySelector('#hour-5');
+  var nm17 = '17';
+
+  function timeCheck17() {
+    if (nm17 < dayjs().format('H')) {
+      text17.classList.add('past')
+      // document.getElementById('#hour-12').className = 'past row time-block';
+      // document.getElementById('#hour-12').setAttribute('class', '.future');
+      return
+    }
+    else if (nm17 === dayjs().format('H')) {
+      text17.classList.add('present')
+      // document.getElementById('#hour-12').className = 'present row time-block';   
+      return   
+    }
+    else if (nm17 > dayjs().format('H')){
+      text17.classList.add('future')
+      // document.querySelector('#hour-12').className = 'future row time-block';
+      return
+    }
+  }
+
+
   timeCheck9();
-  // timeCheck10();
+  timeCheck10();
   timeCheck11();
   timeCheck12();
+  timeCheck13();
+  timeCheck14();
+  timeCheck15();
+  timeCheck16();
+  timeCheck17();
+
+
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
